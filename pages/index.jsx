0987b3d4b983b3c1db529a1edb3e807ed9cfb0ec
Suellen Rosemberg <suellenrosemberg10@gmail.com>
2022-08-10 15:00:18 -0400

    gg

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,9 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <h1>Essa é a página principal</h1>
+      <h1>Informações enviadas</h1>
       <div>
-        <span className={styles.text}>Esse é um texto da Div</span>
+        <span className={styles.text}>OK</span>
       </div>
     </>
   );
